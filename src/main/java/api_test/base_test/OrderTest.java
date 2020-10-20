a5a65634a5d98a913141fc26aec1fa735defaefc
Orangeyangcheng <1178624121@qq.com>
2020-10-20 16:31:43 +0800

    修改房源脚本

--- src/main/java/api_test/base_test/OrderTest.java
+++ src/main/java/api_test/base_test/OrderTest.java
@@ -3,13 +3,17 @@ package api_test.base_test;
 
 import api_test.lease.LeaseBO;
 import api_test.uac.UserBO;
+import com.sun.xml.internal.stream.events.NamedEvent;
 import common.DataSupport;
 import common.HttpRequest;
 import common.HttpUtil;
 import mybatis.pojo.HouseRoom;
 import net.sf.json.JSONObject;
+import org.apache.commons.lang.StringUtils;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static common.BeautifyJson.beautifyJson;
@@ -43,8 +47,8 @@ public class OrderTest {
 
         String rep = HttpUtil.doPost(httpRequest);
         System.out.println("========================"+"新签初始化信息"+"========================");
-        JSONObject repJson = JSONObject.fromObject( rep );
-        System.out.println(beautifyJson(repJson));
+        JSONObject xqRepJson = JSONObject.fromObject( rep );
+        System.out.println(beautifyJson(xqRepJson));
 
         JSONObject leaseParam = new JSONObject();
         leaseParam.put( "signType",leaseBO.getSignType());
@@ -52,8 +56,85 @@ public class OrderTest {
         leaseParam.put( "roomCode",houseRoom.getRoomCode());
         leaseParam.put( "houseType",houseRoom.getHouseMode());
 
+        JSONObject lessee = new JSONObject();
+        lessee.put( "realName",leaseBO.getRealName() );
+        lessee.put( "cardNo",leaseBO.getCardNo() );
+        lessee.put( "mobile",leaseBO.getMobile() );
+        lessee.put( "cardType",leaseBO.getCardType() );
+        lessee.put( "sex",1 );
+        lessee.put( "birthday","" );
+        leaseParam.put( "lessee",lessee);
+        leaseParam.put( "startDate",leaseBO.getStartDate());
+        if (StringUtils.isNotBlank(leaseBO.getEndDate())){
+            leaseParam.put( "endDate",leaseBO.getEndDate() );
+        }
+        leaseParam.put( "rentType",1 );
+        leaseParam.put( "rentWay",1 );
+        leaseParam.put( "rentFee",leaseBO.getRentFee() );
+        leaseParam.put( "rentFeeStr",leaseBO.getRentFee());
+        leaseParam.put( "rentMonth",leaseBO.getRentMonth());
+        leaseParam.put( "salesmanId",userBO.getUserId());
+        leaseParam.put( "salesmanName",userBO.getUserName());
+        leaseParam.put( "remark","自动化测试租约，编号:"+System.currentTimeMillis());
+        leaseParam.put( "shackUpPersons",new ArrayList<String>() );
 
-        return repJson;
+        List leaseDepositCosts = new ArrayList();
+        JSONObject xqRepJsonData = xqRepJson.getJSONObject( "data" );
+        List leaseDepositDicts = xqRepJsonData.getJSONArray( "leaseDepositDicts" );
+        JSONObject leaseDepositDict = (JSONObject) leaseDepositDicts.get( 0 );
+
+        JSONObject leaseDepositCost = new JSONObject();
+        leaseDepositCost.put( "itemName", leaseDepositDict.getString( "dictItemName" ));
+        leaseDepositCost.put( "itemCode", leaseDepositDict.getString( "dictItemValue" ));
+        leaseDepositCost.put( "typeCode", leaseDepositDict.getString( "dictTypeCode" ));
+        leaseDepositCost.put( "itemFeeStr", leaseBO.getDepositFee());
+        leaseDepositCost.put( "rentDepositType", 1 );
+        leaseDepositCost.put( "depositMonthNum", 1);
+        leaseDepositCost.put( "isDefault", true);
+        leaseDepositCosts.add( leaseDepositCost );
+
+        leaseParam.put( "leaseDepositCosts",leaseDepositCosts);
+
+        List leaseOtherCosts = new ArrayList();
+        leaseParam.put( "leaseOtherCosts",leaseOtherCosts);
+        List leaseIrregularRents = new ArrayList();
+        leaseParam.put( "leaseIrregularRents",leaseIrregularRents);
+
+        List leaseRenters = new ArrayList();
+        JSONObject leaseRenter = new JSONObject();
+        leaseRenter.put( "renterType",1 );
+        leaseRenter.put( "cardNo",leaseBO.getCardNo() );
+        leaseRenter.put( "cardType",1 );
+        leaseRenter.put( "mobile",leaseBO.getMobile() );
+        leaseRenter.put( "realName",leaseBO.getRealName() );
+        leaseRenter.put( "birthday","" );
+        leaseRenter.put( "sex",1 );
+        List paperworkUrlList = new ArrayList();
+        leaseRenter.put( "paperworkUrlList",paperworkUrlList );
+
+        List contractPicUrlList = new ArrayList();
+        leaseParam.put( "contractPicUrlList",contractPicUrlList);
+
+        leaseParam.put( "tenantSourceCode","1");
+        leaseParam.put( "leaseIntentCode","84");
+        leaseParam.put( "currentRentType","1");
+        leaseParam.put( "roomPriceStr",xqRepJsonData.getString( "roomPriceStr" ));
+        leaseParam.put( "defaultSignType",1);
+        leaseParam.put( "defaultLeaseIntentDictCode","");
+        leaseParam.put( "rentWayInits",xqRepJsonData.getJSONObject( "rentWayInits" ));
+        leaseParam.put( "contractPicUrlList",contractPicUrlList);
+        leaseParam.put( "contractPicUrlList",contractPicUrlList);
+        leaseParam.put( "contractPicUrlList",contractPicUrlList);
+        leaseParam.put( "contractPicUrlList",contractPicUrlList);
+        leaseParam.put( "contractPicUrlList",contractPicUrlList);
+        leaseParam.put( "contractPicUrlList",contractPicUrlList);
+        leaseParam.put( "contractPicUrlList",contractPicUrlList);
+        leaseParam.put( "contractPicUrlList",contractPicUrlList);
+        leaseParam.put( "contractPicUrlList",contractPicUrlList);
+        leaseParam.put( "contractPicUrlList",contractPicUrlList);
+        leaseParam.put( "contractPicUrlList",contractPicUrlList);
+
+        return xqRepJson;
     }
 
     public static void main(String[] args) {
--- src/main/java/api_test/lease/LeaseBO.java
+++ src/main/java/api_test/lease/LeaseBO.java
@@ -10,6 +10,76 @@ public class LeaseBO {
 
     private int signType;
 
+    private int cardType;
+
+    private String startDate;
+
+    private String endDate;
+
+    private int months;
+
+    private String rentFee;
+
+    private String depositFee;
+
+    private String rentMonth;
+
+    public String getRentMonth() {
+        return rentMonth;
+    }
+
+    public void setRentMonth(String rentMonth) {
+        this.rentMonth = rentMonth;
+    }
+
+    public String getRentFee() {
+        return rentFee;
+    }
+
+    public void setRentFee(String rentFee) {
+        this.rentFee = rentFee;
+    }
+
+    public String getDepositFee() {
+        return depositFee;
+    }
+
+    public void setDepositFee(String depositFee) {
+        this.depositFee = depositFee;
+    }
+
+    public int getMonths() {
+        return months;
+    }
+
+    public void setMonths(int months) {
+        this.months = months;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public int getCardType() {
+        return cardType;
+    }
+
+    public void setCardType(int cardType) {
+        this.cardType = cardType;
+    }
+
     public int getSignType() {
         return signType;
     }
